b9158820b73451af8c9f6b632a70abc934eb3092
Maxime Louis <maxime.louis@telecomnancy.eu>
2017-09-16 14:45:23 +0200

    server

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var compression = require('compression');
-var jade = require("jade");
+var express = require("express");
 var app = express();
 
+var path = __dirname+"/public/templates/";
+
 app.use(compression());
+app.use(express['static'](__dirname + '/public'));
 
-app.set('view engine', 'jade');
-	
-
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/',function(req, res){  
+      res.sendFile(path + 'index.html');
 });
-
 
 app.listen(80, function () {
 	console.log("YumHax")
 });
+ 
+
+
 
